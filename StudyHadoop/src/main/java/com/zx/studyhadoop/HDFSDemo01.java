de785ea1df39a5e473b16ca2b8a449ea4f725f14
zhaoxing <1193166256@qq.com>
2017-07-11 11:41:45 +0800

    midify HDFSDemo01.java

--- StudyHadoop/src/main/java/com/zx/studyhadoop/HDFSDemo01.java
+++ StudyHadoop/src/main/java/com/zx/studyhadoop/HDFSDemo01.java
@@ -1,13 +1,11 @@
 package com.zx.studyhadoop;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URISyntaxException;
 
 import org.apache.commons.compress.utils.IOUtils;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
@@ -18,19 +16,15 @@ public class HDFSDemo01 {
 		try {
 			URI uri = new URI("hdfs://master:9000"); //hadoop分布式文件系统master所在的uri
 			FileSystem fs = FileSystem.get(uri,conf,"hadoop");//根据配置和远程地址创建文件系统
-			
+//			FileSystem fs = FileSystem.newInstance(conf);
 			Path file = new Path("/data/1.sh");
 			InputStream in = fs.open(file);
 			IOUtils.copy(in, System.out, 1024);
 			
+			FileStatus status = fs.getFileStatus(file);
+			System.out.println(status.getGroup());
 			
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} catch (URISyntaxException e) {
+		}catch (Exception e) {
 			e.printStackTrace();
 		}
 		

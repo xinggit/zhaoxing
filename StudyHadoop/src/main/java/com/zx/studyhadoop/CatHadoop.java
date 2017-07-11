2aa26813d327d4d71012cef989b0cc61cdca826a
zhaoxing <1193166256@qq.com>
2017-07-11 09:08:58 +0800

    modify CatHadoop

--- StudyHadoop/src/main/java/com/zx/studyhadoop/CatHadoop.java
+++ StudyHadoop/src/main/java/com/zx/studyhadoop/CatHadoop.java
@@ -1,29 +1,41 @@
 package com.zx.studyhadoop;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
+import org.apache.hadoop.io.IOUtils;
 
 public class CatHadoop {
 	public static void main(String[] args) {
 		URL url = null;
-		
+		URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+//		try {
+//			url = new URL("hdfs://master:9000/data/1.sh");
+//			URLConnection con = url.openConnection();
+//			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"));
+//			String line = null;
+//			while((line = in.readLine()) != null) {
+//				System.out.println(line);
+//			}
+//			
+//			
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 		try {
-			URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
 			url = new URL("hdfs://master:9000/data/1.sh");
 			URLConnection con = url.openConnection();
-			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"));
-			String line = null;
-			while((line = in.readLine()) != null) {
-				System.out.println(line);
-			}
+//			IOUtils.copyBytes(con.getInputStream(), System.out, 2048, true);
+			IOUtils.copyBytes(con.getInputStream(), System.out, 1024, true);
 			
-			
-		} catch (Exception e) {
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 	}
 }

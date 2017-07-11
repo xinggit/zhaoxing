50cd503edfacd84624290da0959b5a31b36b2c97
赵星 <1193166256@qq.com>
2017-07-11 10:12:32 +0800

    编写HDFSDemo01

--- StudyHadoop/src/main/java/com/zx/studyhadoop/HDFSDemo01.java
+++ StudyHadoop/src/main/java/com/zx/studyhadoop/HDFSDemo01.java
@@ -1,21 +1,27 @@
 package com.zx.studyhadoop;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 
+import org.apache.commons.compress.utils.IOUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
 public class HDFSDemo01 {
 	public static void main(String[] args) {
-		Configuration conf = new Configuration();
+		Configuration conf = new Configuration();//hadoop配置文件对象
 		
 		try {
-			URL url = new URL("hdfs://master:9000");
-			FileSystem.get(url.toURI(),conf,"hadoop");//
+			URI uri = new URI("hdfs://master:9000"); //hadoop分布式文件系统master所在的uri
+			FileSystem fs = FileSystem.get(uri,conf,"hadoop");//根据配置和远程地址创建文件系统
 			
+			Path file = new Path("/data/1.sh");
+			InputStream in = fs.open(file);
+			IOUtils.copy(in, System.out, 1024);
 			
 			
 		} catch (MalformedURLException e) {
